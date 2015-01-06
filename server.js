ce97051143cf3ee1804d48cf34150ab486c1ebdf
Jason Miller <jason@developit.ca>
2015-01-06 15:36:52 -0500

    Update server

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express'),
+	compress = require('compression'),
+	serveStatic = require('serve-static'),
+	app = express(),
+	port = process.env.PORT || 8080;
+
+app.use(compress());
+
+// rewrite
+app.use(function(req, res, next) {
+	if (!req.url.match(/(^\/?(static|docs|tests)\/|\.[a-z]+$)(\?.*)?/g)) {
+		req.url = '/';
+	}
+	next();
+});
+
+app.use(serveStatic('docs'));
+
+app.use('/demo', serveStatic('demo'));
+
+app.use('/dist', serveStatic('dist'));
+
+app.listen(port, function() {
+	console.log('Server listening on localhost:'+port);
+});
