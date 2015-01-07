ec108ea03c96ff6c826460fe7ce983af67b5adc3
Jason Miller <jason@developit.ca>
2015-01-07 14:38:32 -0500

    Fix URL redirection in dev server

--- server.js
+++ server.js
@@ -1,24 +1,21 @@
 var express = require('express'),
-	compress = require('compression'),
+	compression = require('compression'),
 	serveStatic = require('serve-static'),
 	app = express(),
 	port = process.env.PORT || 8080;
 
-app.use(compress());
+app.use(compression());
 
-// rewrite
 app.use(function(req, res, next) {
-	if (!req.url.match(/(^\/?(static|demo|docs|dist|tests)\/|\.[a-z]+$)(\?.*)?/g)) {
-		req.url = '/';
+	if (req.url.match(/^\/?demo(\/|$)/g) && !req.url.match(/\.[a-z]+(\?.*)?$/g)) {
+		req.url = '/demo/';
 	}
 	next();
 });
 
-app.use(serveStatic('docs'));
-
-app.use('/demo', serveStatic('demo'));
-
-app.use('/dist', serveStatic('dist'));
+app.use(serveStatic('docs/'));
+app.use('/dist', serveStatic('dist/'));
+app.use('/demo', serveStatic('demo/'));
 
 app.listen(port, function() {
 	console.log('Server listening on localhost:'+port);
